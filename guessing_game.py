51acce72c991f62c20361fd7010b7d1f2bcc8269
sanuavi29 <sanu2003avi@gmail.com>
2025-05-03 21:28:19 +0530

    ddd

--- guessing_game.py
+++ guessing_game.py
@@ -3,6 +3,6 @@ number = random.randint(1, 10)
 print("Guess a number between 1 and 10")
 guess = int(input())
 if guess == number:
-    print("You winbbb!")
+    print("You winbbb!2222")
 else:
     print(f"Wrong! The number was {number}")
