ea92a6c3715da6220a4afe5fe552e7b98b9b9173
Maheshika2001 <maheshibanagala01@gmail.com>
2025-05-03 21:51:50 +0530

    Increase number range to 1-100

--- guessing_game.py
+++ guessing_game.py
@@ -1,6 +1,6 @@
 import random
-number = random.randint(1, 10)
-print("Guess a number between 1 and 10")
+number = random.randint(1, 100)
+print("Guess a number between 1 and 100")
 guess = int(input())
 if guess == number:
     print("You winbbb!2222")
