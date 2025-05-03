4b48b2c1040aeb415a955059775bce30f90c537e
sanuavi29 <sanu2003avi@gmail.com>
2025-05-03 22:38:15 +0530

    "Add hint messages for guesses"

--- guessing_game.py
+++ guessing_game.py
@@ -1,8 +1,19 @@
 import random
-number = random.randint(1, 10)
-print("Guess a number between 1 and 10")
-guess = int(input())
-if guess == number:
-    print("You winbbb!2222")
-else:
-    print(f"Wrong! The number was {number}")
+
+while True:
+    number = random.randint(1, 100)
+    print("Guess a number between 1 and 100")
+    guess = int(input())
+    if guess < number:
+    print("Too low!")
+elif guess > number:
+    print("Too high!")
+
+    if guess == number:
+        print("You win!")
+    else:
+        print(f"Wrong! The number was {number}")
+    
+    print("Play again? (y/n)")
+    if input().lower() != 'y':
+        break
